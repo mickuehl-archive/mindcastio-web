b0f7dcc1031e14146d316fc488a4842e865b3202
Michael Kuehl <michael@ratchet.cc>
2016-03-09 07:53:21 +0100

    initial

---
+++ app/controllers/visitors_controller.rb
@@ -0,0 +1,7 @@
+class VisitorsController < ApplicationController
+
+  def index
+    render layout: "visitors"
+  end
+  
+end
---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,11 @@
+module ApplicationHelper
+
+  def application_name
+    "BASE"
+  end
+
+  def application_description
+    "BASE app"
+  end
+
+end
