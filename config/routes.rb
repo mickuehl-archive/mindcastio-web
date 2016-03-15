8d7d824cf33bce95660e5df608672db70eb74138
Michael Kuehl <michael@ratchet.cc>
2016-03-15 18:36:01 +0100

    renamed the basic views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+
   root to: 'search#index'
 
   post '/search/:page', :to => 'search#search'
+
+  get '/p/:uid', :to => 'podcast#podcast'
+  get '/e/:uid', :to => 'podcast#episode'
 
 end
